985bd90f394427f5ec92d76a7008715ed11a326a
subeshhub <nunusubbu21@gmail.com>
2026-02-12 07:12:12 +0530

    task3

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # daily\_task
 
 daily tasks on python basics
+task3.py
 
 
 
 
+task\_3
 
-task\_2
-
-i learn how python run useing integers and complex numbers and floting points
+i learn how to store the numbers and floting points and complex numbers list, tuple in python useing syntax
 
 
---
+++ task3.py
@@ -0,0 +1,30 @@
+# student name
+stu_name= "subesh"
+print(stu_name)
+print(type(stu_name))
+
+# student age
+age=21
+print(age)
+print(type(age))
+
+# student markes in degree
+marks_CGP=8.5
+print(marks_CGP)
+print(type(marks_CGP))
+
+
+# complex result
+complex_value=85+100j
+print(complex_value)
+print(type(complex_value))
+
+# subject in exam
+subject=["maths","commers","econamics"]
+print(subject)
+print(type(subject))
+
+#date of birth of student
+DOB=(21,7,2005)
+print(DOB)
+print(type(DOB))
